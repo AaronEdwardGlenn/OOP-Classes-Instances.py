a608caaff43925f2b647552bf0aea929cb5a9111
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-26 21:08:26 -0700

    minor refactor

--- classes_instances.py
+++ classes_instances.py
@@ -27,31 +27,3 @@ emp_2.pay = 1111100000
 
 print(emp_1.email)
 print(emp_2.email)
-
-# now we are going to make this more readable with less code
-
-
-class Employee1:
-
-    def __init__(self)
-
-
-emp_11 = Employee1()
-emp_22 = Employee1()
-
-print(emp_1)
-print(emp_2)
-
-
-emp_1.first = 'Aaron'
-emp_1.last = 'Glenn'
-emp_1.email = 'a@co.com'
-emp_1.pay = 100000
-
-emp_2.first = 'Test'
-emp_2.last = 'User'
-emp_2.email = 'test@co.com'
-emp_2.pay = 1111100000
-
-print(emp_1.email)
-print(emp_2.email)
